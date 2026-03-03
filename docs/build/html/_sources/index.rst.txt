e5766480b4901501ef2f6cfbf076f1b2a150e848
timdazayous <tim.dazayous@gmail.com>
2026-03-03 11:50:51 +0100

    docs: génération de la documentation Sphinx avec Furo

---
+++ docs/build/html/_sources/index.rst.txt
@@ -0,0 +1,17 @@
+Toolbox MLOps
+=============
+
+.. include:: ../../README.md
+   :parser: myst_parser.sphinx_
+
+Documentation du Code (API)
+===========================
+
+.. automodule:: app.modules.mon_module
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Sommaire:
---
+++ docs/source/conf.py
@@ -0,0 +1,36 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../'))
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Toolbox MLOps'
+copyright = '2026, Sidora AI'
+author = 'Sidora AI'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",     # Pour aspirer les docstrings
+    "sphinx.ext.napoleon",    # Pour comprendre le format "Google"
+    "myst_parser",            # Pour lire le README.md
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'fr'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "furo"
+html_static_path = ['_static']
